37139930d8107484b076f45f02b99d895a0f0554
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-07-18 22:47:23 +0700

    refactor tests

--- app/tests/auth_test.py
+++ app/tests/auth_test.py
@@ -1,56 +1,81 @@
+import json
+from unittest.mock import mock_open, patch
+
 import pytest
 
 from app.blueprints.auth import UserDetect
 from app.exceptions import FileCommentsNotExists, NotExpectedType
-from app.paths import USERS_JSON_PATH
-
-
-@pytest.fixture()
-def user_detect():
-    user = UserDetect({'User-Agent': 'test'}, '127.0.0.1', USERS_JSON_PATH)
-    return user
-
-
-@pytest.fixture()
-def break_user_detect():
-    break_user = UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test.json')
-    return break_user
-
-
-@pytest.fixture()
-def break_users_file_bad_content():
-    break_user = UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'app/tests/test_users_bad_content.json')
-    return break_user
 
 
 class TestAuth:
-    def test_get_md5_for_user(self, user_detect):
-        assert isinstance(user_detect.get_md5_for_user(), str), 'не верный тип данных в md5'
-        assert len(user_detect.get_md5_for_user()) == 32, 'длина не соответствует md5'
 
-    def test_load_users(self, user_detect, break_user_detect, break_users_file_bad_content):
-        assert isinstance(user_detect.load_users(), list), 'не верный тип возвращаемых данных'
+    def test_get_md5_for_user(self):
+        assert isinstance(UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_md5_for_user(), str)
 
-        with pytest.raises(FileCommentsNotExists) as exception_err:
-            break_user_detect.load_users()
-        exception_message = exception_err.value.message
-        assert exception_message == 'Фаил c комментариями не найден', 'не верно работает исключение по файлу с users'
+    def test_load_users(self, open_mock):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').load_users() == [{'post_id': 1}]
+        open_mock.side_effect = FileNotFoundError
+        with pytest.raises(FileCommentsNotExists):
+            UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').load_users()
 
-        with pytest.raises(NotExpectedType) as exception_err:
-            break_users_file_bad_content.load_users()
-        exception_message = exception_err.value.message
-        assert exception_message == 'Не верный формат данных', 'не верно работает исключение по содержимому файла с ' \
-                                                               'users '
+    @pytest.mark.parametrize('data', {'id': 1, 'name': 'ivan'})
+    def test_load_users_2(self, data):
+        with pytest.raises(NotExpectedType):
+            with patch('builtins.open', new_callable=mock_open, read_data=json.dumps(data)):
+                UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').load_users()
 
-    def test_get_all_users_md5(self, user_detect):
-        assert isinstance(user_detect.get_all_users_md5(), list), 'не верный тип возвращаемых данных'
+    def test_write_user(self, open_mock):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').write_user([{'post_id': 2}]) is None
 
-    # def test_get_like(self, users_file):
-    #     assert users_file.get_like(1) == False
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93"}])
+    def test_get_all_users_md5(self, load_users):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_all_users_md5() == ["cca173be1c70b68f"
+                                                                                               "7852b7aaf8a27f93"]
 
+    @patch('app.blueprints.auth.UserDetect.get_md5_for_user', return_value="cca173be1c70b68f7852b7aaf8a27f93")
+    @patch('app.blueprints.auth.UserDetect.get_all_users_md5', return_value=["cca173be1c70b68f7852b7aaf8a27f93"])
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93",
+                                                                       "pk_posts_with_like": [],
+                                                                       "pk_posts_in_bookmarks": [],
+                                                                       }])
+    def test_add_new_user(self, load_users, get_md5_for_user, get_all_users_md5, open_mock):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').add_new_user() is None
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_like(1) is True
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_bookmark(1) is True
 
+    @patch('app.blueprints.auth.UserDetect.get_md5_for_user', return_value="gfhjbnk")
+    @patch('app.blueprints.auth.UserDetect.get_all_users_md5', return_value=["cca173be1c70b68f7852b7aaf8a27f93"])
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93",
+                                                                       "pk_posts_with_like": [1],
+                                                                       "pk_posts_in_bookmarks": [1],
+                                                                       }])
+    def test_add_new_user_2(self, load_users, get_md5_for_user, get_all_users_md5, open_mock):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').add_new_user() is None
 
-# @mock.patch('app.blueprints.auth.UserDetect.load_users')
-# def test_load_users_type(mock_load_users: mock.Mock, user_detect):
-#     mock_load_users.return_value = 5
-#     assert user_detect.load_users() == 5
+    @patch('app.blueprints.auth.UserDetect.get_md5_for_user', return_value="cca173be1c70b68f7852b7aaf8a27f93")
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93",
+                                                                       "pk_posts_with_like": [1],
+                                                                       "pk_posts_in_bookmarks": [1],
+                                                                       }])
+    def test_add_new_user_3(self, load_users, get_md5_for_user, open_mock):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_like(1) is False
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_bookmark(1) is False
+
+    @patch('app.blueprints.auth.UserDetect.get_md5_for_user', return_value="gfhjbnk")
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93",
+                                                                       "pk_posts_with_like": [1],
+                                                                       "pk_posts_in_bookmarks": [1],
+                                                                       }])
+    def test_add_new_user_4(self, load_users, get_md5_for_user):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_like(1) is None
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_bookmark(1) is None
+
+    @patch('app.blueprints.auth.UserDetect.get_all_users_md5', return_value=["cca173be1c70b68f7852b7aaf8a27f93"])
+    @patch('app.blueprints.auth.UserDetect.get_md5_for_user', return_value="cca173be1c70b68f7852b7aaf8a27f93")
+    @patch('app.blueprints.auth.UserDetect.load_users', return_value=[{"user_md5": "cca173be1c70b68f7852b7aaf8a27f93",
+                                                                       "pk_posts_with_like": [1],
+                                                                       "pk_posts_in_bookmarks": [1],
+                                                                       }])
+    def test_get_likes_list(self, load_users, get_md5_for_user, get_all_users_md5):
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_likes_list() == [1]
+        assert UserDetect({'User-Agent': 'test'}, '127.0.0.1', 'test').get_bookmarks_list() == [1]
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,6 @@
+import json
+from unittest.mock import patch, mock_open
+
 import pytest
 
 from app.app import app
@@ -6,3 +9,9 @@ from app.app import app
 @pytest.fixture()
 def test_client():
     return app.test_client()
+
+
+@pytest.fixture()
+def open_mock():
+    with patch('builtins.open', new_callable=mock_open, read_data=json.dumps([{'post_id': 1}])) as mock:
+        yield mock
